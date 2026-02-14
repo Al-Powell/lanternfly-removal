792ddfb933ae568bad818bfef356744395a907bd
Alora <a.tan.powell@gmail.com>
2026-02-14 15:09:02 -0500

    info button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import Button from '@mui/joy/Button';
+import Input from '@mui/joy/Input';
 export default function Home() {
   const [toggleCreate, setCreateToggle] = useState(-1);
   const [toggleInfo, setInfoToggle] = useState(-1);
@@ -19,11 +21,12 @@ export default function Home() {
           <h1>Create New Group</h1>
         </button>
       { toggleCreate === 1&& (
-        <div>
+        <div style={{display: "flex", flexDirection: "column", gap: "10px"}}>
           <label>Location:
-           <input name = "Enter Location"></input>
-           <button onClick={() => getUserLocation()}>Use Current Location</button>
+           <Input name = "Enter Location"></Input>
+           <Button variant="solid" onClick={() => setToggle((toggle) => toggle * -1)}>Use Current Location</Button>
            </label>
+           <Input name = "How many people do you need?"/>
            <label>
               How many people do you need?
               <input name = "Enter a number"></input>
